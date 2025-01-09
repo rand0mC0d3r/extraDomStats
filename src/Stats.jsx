9b5759848c9a0208363a7b4cde6d0f8389ef9e0b
rand0mC0d3r <practiseo@gmail.com>
2025-01-09 15:28:22 +0100

    Refactor Stats component styling and layout; enhance wrapper and root element properties for improved visual structure

--- src/Stats.jsx
+++ src/Stats.jsx
@@ -7,31 +7,48 @@ import './googleMaps.css';
 import theme from './theme';
 import { extensionIdentifier } from './utils';
 
-// // Create a wrapper div
-// const wrapper = document.createElement("div");
+
+const heightOfBar = '36px';
+
+// Create a wrapper div
+const wrapper = document.createElement("div");
 // wrapper.style.position = "relative";
+wrapper.style.position = "relative";
+// wrapper.style.backgroundColor = 'blue';
+wrapper.style.border = '5px solid yellow';
+wrapper.style.flex = '1 1 auto';
+
+wrapper.style.padding = '10px';
+wrapper.style.margin = '10px';
+wrapper.style.height = '90%';
+wrapper.style.maxHeight = '90%';
 // wrapper.style.width = "100%";
 // wrapper.style.height = "100%";
-// wrapper.style.overflow = "hidden";
-// wrapper.id = `${extensionIdentifier}-wrapper`;
+wrapper.style.overflow = "scroll";
+wrapper.id = `${extensionIdentifier}-wrapper`;
 
-// // Move all existing body content into the wrapper
-// while (document.body.firstChild) {
-//   wrapper.appendChild(document.body.firstChild);
-// }
+// Move all existing body content into the wrapper
+while (document.body.firstChild) {
+  wrapper.appendChild(document.body.firstChild);
+}
 
-// // Append the wrapper to the body
-// document.body.appendChild(wrapper);
+// Append the wrapper to the body
+document.body.appendChild(wrapper);
 
 // Create the root div
 const root = document.createElement("div");
-root.style.top = "0";
-root.style.left = "0";
-root.style.right = "0";
-root.style.bottom = "0";
-root.style.zIndex = "2147483647";
+// root.style.top = "0";
+// root.style.left = "0";
+// root.style.right = "0";
+// root.style.bottom = "0";
+// root.style.zIndex = "2147483647";
+root.style.backgroundColor = 'red';
+root.style.flex = '1 1 auto';
+root.style.height = heightOfBar;
+root.style.minHeight = heightOfBar;
+root.style.maxHeight = heightOfBar;
 // root.style.pointerEvents = "none";
-root.style.position = "fixed";
+// root.style.position = "fixed";
 root.id = extensionIdentifier;
 
 // Append the root to the wrapper
@@ -50,7 +67,7 @@ ReactDOM.createRoot(root).render(<ThemeProvider theme={theme}>
     size="medium"
     hasBorder={true}
     fullWidth={true}
-    style={{ height: '36px', pointerEvents: 'auto' }}
+    style={{ height: heightOfBar, pointerEvents: 'auto' }}
     slim={true}
     variant='outlined'
   >
